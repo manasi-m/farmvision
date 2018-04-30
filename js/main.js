49901ccfaa0149728d7958835d5b220261ffa806
manasi-m <cyber.manasi@gmail.com>
2018-04-30 13:54:12 -0400

    new images / pages

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// initialize side-navigation bar
+
+var elem_sn = document.querySelector('.sidenav');
+var instance_sn = M.Sidenav.init(elem_sn);
+
+
+var elem_col = document.querySelector('.collapsible');
+var instance_col = M.Collapsible.init(elem_col);
+
+var elem = document.querySelector('.tabs');
+var instance = M.Tabs.init(elem);
